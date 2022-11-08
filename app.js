e9a3f7c8ad03e453a836cd3a73bd933bfb380674
Pranav <pranav@v.pranav>
2022-11-08 22:59:37 +0530

    port -> PORT

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use('/images', express.static('images'));
 app.use(express.json());
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 // DB connection
 const dbConnection = require('./utils/db');
