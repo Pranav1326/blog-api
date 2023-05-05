b7efd058cd310b3c4b86ddd824ca1c1b8f568910
Pranav1326 <visavadiapa@gmail.com>
2023-05-05 15:03:05 +0530

    Main branch complete

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 mongoose.set('strictQuery', false);
 
-const dbConnection = mongoose.connect(`mongodb+srv://${process.env.MONGODB_PROFILE}:${process.env.MONGODB_PASSWORD}@cluster0.5kh5v.mongodb.net/?retryWrites=true&w=majority`,
+const dbConnection = mongoose.connect(`mongodb+srv://${process.env.MONGODB_PROFILE}:${process.env.MONGODB_PASSWORD}@cluster0.5kh5v.mongodb.net/blog?retryWrites=true&w=majority`,
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
